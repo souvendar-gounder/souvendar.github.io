82cb9e0bd526994f115a504c4846db9a41c2151f
souvendar-gounder <gpromod7@gmail.com>
2026-02-18 00:09:07 +0530

    Update utility.js

--- js/utility.js
+++ js/utility.js
@@ -1,24 +1,3 @@
-function getLastPartOfURL(url) {
-    return url.substring(url.lastIndexOf('/') + 1);
-}
-
-function currentMenu(){
-    const url = window.location.href;
-const lastPart = getLastPartOfURL(url).toLowerCase();
-console.log(lastPart);
-
-let navs = document.querySelectorAll('.nav-link');
-
-navs.forEach(nav => {
-    nav.classList.remove('active');
-    if(lastPart==nav.id){
-        nav.classList.add('active');
-    }
-});
-
-}
-currentMenu();
-
 document.getElementById('hmb').addEventListener('click',function(){
     let element = document.getElementById("navigation");
     if (!element.classList.contains("show")) {
@@ -34,4 +13,5 @@ document.getElementById('hmb').addEventListener('click',function(){
         element.classList.remove("show");
     }
 })
+
 
